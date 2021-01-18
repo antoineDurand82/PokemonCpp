dccc79d115590525ce3ba5a4300dc836a11612db
Antoine.D <antoine.durand@ynov.com>
2021-01-18 10:23:41 +0100

    Initial CPP commit

---
+++ cmake-build-debug/CMakeFiles/PokemonCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokemonCpp.dir/main.cpp.obj"
+  "PokemonCpp.exe"
+  "PokemonCpp.exe.manifest"
+  "PokemonCpp.pdb"
+  "libPokemonCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PokemonCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
