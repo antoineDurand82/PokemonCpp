966279dd5a88c938e9269b1f5a6f8a63aaa1ad9c
RemiFeydit <remi.feydit@ynov.com>
2021-02-16 16:55:12 +0100

    capture method added

--- Classes/Items/Pokeballs/BallItem.cpp
+++ Classes/Items/Pokeballs/BallItem.cpp
@@ -3,6 +3,33 @@
 //
 
 #include "BallItem.h"
+#include <iostream>
 
 BallItem::BallItem(int id, const std::string &name, int price, int catchRate) : Item(id, name, price),
                                                                                 catchRate(catchRate) {}
+bool BallItem::tryCatch(Pokemon pokemon) {
+    if(pokemon.getIsCaptured()){
+        std::cout << "Ce pokémon ne peut être capturé, il appartient déjà un dresseur." << std::endl;
+        return false;
+    }
+    if(catchRate == 100){
+        pokemon.setIsCaptured(true);
+        return true;
+    }
+    int catchPercent = rand() % 100 + 1;
+    if(catchRate > catchPercent){
+        return true;
+    }
+    return false;
+}
+
+void BallItem::use(Pokemon pokemon) {
+    std::cout << name << " est lancé sur " << pokemon.getName() << "..." << std::endl;
+    bool canCatch = tryCatch(pokemon);
+
+    if(!canCatch){
+        std::cout << "La capture a échoué." << endl;
+    }else{
+        std::cout << pokemon.getName() << " a été capturé !" << std::endl;
+    }
+}
--- Classes/Items/Pokeballs/BallItem.h
+++ Classes/Items/Pokeballs/BallItem.h
@@ -5,12 +5,15 @@
 #ifndef POKEMONCPP_BALLITEM_H
 #define POKEMONCPP_BALLITEM_H
 #include "../Item/Item.h"
+#include "../../Pokemon/Pokemon.h"
 
 class BallItem : public Item {
 private:
     int catchRate;
 public:
     BallItem(int id, const std::string &name, int price, int catchRate);
+    bool tryCatch(Pokemon pokemon);
+    void use(Pokemon pokemon);
 };
 
 
