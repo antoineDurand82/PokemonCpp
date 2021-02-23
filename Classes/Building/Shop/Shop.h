e7a47d4a481fad6b8261cc01af2b6de50958412f
RemiFeydit <remi.feydit@ynov.com>
2021-02-23 12:01:21 +0100

    start building

---
+++ Classes/Building/Shop/Shop.h
@@ -0,0 +1,16 @@
+//
+// Created by notitou on 16/02/2021.
+//
+
+#ifndef POKEMONCPP_SHOP_H
+#define POKEMONCPP_SHOP_H
+
+
+#include "../Building.h"
+
+class Shop : public Building {
+    vector<Item> stock;
+};
+
+
+#endif //POKEMONCPP_SHOP_H
