1dc23c64188646bd9de9fa9d581575f80a7b48e5
Antoine.D <antoine.durand@ynov.com>
2021-02-23 11:55:27 +0100

    add trainer

---
+++ Classes/EnumLocation/Location.cpp
@@ -0,0 +1,21 @@
+//
+// Created by antoi on 15/02/2021.
+//
+
+#include "Location.h"
+
+const std::string &Location::getName() const {
+    return name;
+}
+
+void Location::setName(const std::string &name) {
+    Location::name = name;
+}
+
+const std::vector<Pokemon> &Location::getListPokemon() const {
+    return listPokemon;
+}
+
+void Location::setListPokemon(const std::vector<Pokemon> &listPokemon) {
+    Location::listPokemon = listPokemon;
+}
